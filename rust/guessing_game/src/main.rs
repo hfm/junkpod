8d8c1259f378e95f4e37f4deb6c7876285c99863
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-07-04 09:05:05 +0900

    loop

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -11,21 +11,23 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Pleas input your guess.");
+    loop {
+        println!("Pleas input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => println!("You win!"),
+        }
     }
 }
