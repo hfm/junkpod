890455cc9525d1b861fd9668dc4f2ed971cbbe77
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-07-04 09:03:00 +0900

    match

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,12 +1,13 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 100);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
 
@@ -17,5 +18,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
 }
