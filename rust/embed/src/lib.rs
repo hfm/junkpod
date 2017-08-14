4cd6f031046c0120dbefcac12d8627184e64f415
OKUMURA Takahiro <hfm.garden@gmail.com>
2017-08-14 14:53:11 +0900

    spinup 10threads

--- rust/embed/src/lib.rs
+++ rust/embed/src/lib.rs
@@ -1,6 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+use std::thread;
+
+fn process() {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in 0..5_000_000 {
+                x += 1
+            }
+            x
+        })
+    }).collect();
+
+    for h in handles {
+        println!("Thread finished with count={}",
+                 h.join().map_err(|_| "Could not join a thread!").unwrap());
     }
 }
