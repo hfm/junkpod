a9bec155024062e53fb817073e89baa38d0aa830
OKUMURA Takahiro <hfm.garden@gmail.com>
2017-10-09 21:23:30 +0900

    Rust.new

--- rust/ruby-native-ext-via-rust/src/lib.rs
+++ rust/ruby-native-ext-via-rust/src/lib.rs
@@ -1,6 +1,18 @@
-#![allow(non_snake_case)]
+#![allow(non_snake_case, non_camel_case_types)]
+
+extern crate libc;
+use std::ffi::CString;
+
+type VALUE = libc::c_ulong;
+
+extern {
+    static rb_cObject: VALUE;
+    fn rb_define_class(name: *const libc::c_char, rb_super: VALUE) -> VALUE;
+}
 
 #[no_mangle]
 pub extern fn Init_rust() {
-    println!("Init_rust");
+    let c_name = CString::new("Rust").unwrap();
+
+    unsafe { rb_define_class(c_name.as_ptr(), rb_cObject) };
 }
