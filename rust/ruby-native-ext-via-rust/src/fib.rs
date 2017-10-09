84af719829f95d5c6a21a52234345b65704d83af
OKUMURA Takahiro <hfm.garden@gmail.com>
2017-10-09 21:42:03 +0900

    fibonacci

---
+++ rust/ruby-native-ext-via-rust/src/fib.rs
@@ -0,0 +1,3 @@
+pub fn fib(n: u32) -> u32 {
+    if n <= 1 { n } else { fib(n - 1) + fib(n - 2) }
+}
--- rust/ruby-native-ext-via-rust/src/lib.rs
+++ rust/ruby-native-ext-via-rust/src/lib.rs
@@ -1,18 +1,41 @@
 #![allow(non_snake_case, non_camel_case_types)]
 
 extern crate libc;
+mod fib;
+
 use std::ffi::CString;
 
 type VALUE = libc::c_ulong;
 
-extern {
+extern "C" {
     static rb_cObject: VALUE;
     fn rb_define_class(name: *const libc::c_char, rb_super: VALUE) -> VALUE;
+    fn rb_define_method(klass: VALUE,
+                        name: *const libc::c_char,
+                        func: extern "C" fn(v: VALUE, v2: VALUE),
+                        argc: libc::c_int)
+                        -> libc::c_void;
+    fn rb_num2long(val: VALUE) -> libc::c_long;
+}
+
+fn rb_int2fix(num: u32) -> VALUE {
+    return ((num as VALUE) << 1) | 0x01;
+}
+
+extern fn rb_fib(_: VALUE, rb_num: VALUE) {
+    let num = unsafe { rb_num2long(rb_num) } as u32;
+    let result = fib::fib(num);
+
+    rb_int2fix(result);
 }
 
 #[no_mangle]
-pub extern fn Init_rust() {
+pub extern "C" fn Init_rust() {
     let c_name = CString::new("Rust").unwrap();
+    let fib = CString::new("fib").unwrap();
 
-    unsafe { rb_define_class(c_name.as_ptr(), rb_cObject) };
+    unsafe {
+        let rb_cRust = rb_define_class(c_name.as_ptr(), rb_cObject);
+        rb_define_method(rb_cRust, fib.as_ptr(), rb_fib, 1);
+    }
 }
