4e136cedbe6a16e002d69f6443e2cf3ebd65a7e3
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-07-17 05:39:47 +0900

    vue

---
+++ go/test-echo-scaffold/static/app.js
@@ -0,0 +1,55 @@
+ELEMENT.locale(ELEMENT.lang.ja)
+var app = new Vue({
+  el: '#app',
+  data: {
+    tasks: [],
+    newTask: "",
+    loading: false,
+  },
+  created: function() {
+    this.loading = true;
+    axios.get('/tasks')
+      .then((response) => {
+        console.log(response);
+        this.tasks = response.data.tiems || [];
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.loading = false;
+      });
+  },
+  methods: {
+    addTask: function(task) {
+      this.loading = true;
+      let params = new URLSearchParams();
+      params.append('body', this.newTask);
+      params.append('done', false);
+      axios.post('/tasks', params)
+        .then((response) => {
+          this.loading = false;
+          this.tasks.unshift(response.data);
+          this.newTask = "";
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.loading = false;
+        });
+    },
+    doneTask: function(task) {
+      this.loading = true;
+      let params = new URLSearchParams();
+      params.append('done', !task.done);
+      axios.put('/tasks/' + task.id, params)
+        .then((response) => {
+          console.log(response)
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.loading = false;
+        });
+    }
+  }
+})
