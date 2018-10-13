3306d6a17b3c9a5c6a747560964d52c1cb69ad36
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-10-14 02:49:10 +0900

    computed

--- js/vue/vuejs_nyumon/app.js
+++ js/vue/vuejs_nyumon/app.js
@@ -21,6 +21,24 @@ var vm = new Vue({
   el: '#app',
   data: {
     items: items
+  },
+  filters: {
+    numberWithDelimiter: function (value) {
+      if (!value) {
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  computed: {
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function () {
+      return Math.floor(this.totalPrice * 1.08)
+    }
   }
 });
 
