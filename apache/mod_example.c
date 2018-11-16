a0a4f7051cc0269266af3c8d0ad4742f731b3cdf
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-11-16 23:59:40 +0900

    register_hooks

--- apache/mod_example.c
+++ apache/mod_example.c
@@ -1,5 +1,7 @@
 #include "http_core.h"
 
+static void register_hooks(apr_pool_t *pool);
+
 module AP_MODULE_DECLARE_DATA example_module =
 {
     STANDARD20_MODULE_STUFF,
@@ -8,5 +10,11 @@ module AP_MODULE_DECLARE_DATA example_module =
     NULL,
     NULL,
     NULL,
-    NULL
+    register_hooks
 };
+
+static void register_hooks(apr_pool_t *pool)
+{
+    /* Create a hook in the request handelr, so we get called when a request arrives */
+    ap_hook_handler(example_handler, NULL, NULL, APR_HOOK_LAST);
+}
