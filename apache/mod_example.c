b90f199f8255c816179cc3e5fca18cf644807819
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-11-17 00:19:40 +0900

    example_handler

--- apache/mod_example.c
+++ apache/mod_example.c
@@ -1,6 +1,8 @@
 #include "http_core.h"
+#include "http_protocol.h"
 
 static void register_hooks(apr_pool_t *pool);
+static int example_handler(request_rec *r);
 
 module AP_MODULE_DECLARE_DATA example_module =
 {
@@ -17,4 +19,24 @@ static void register_hooks(apr_pool_t *pool)
 {
     /* Create a hook in the request handelr, so we get called when a request arrives */
     ap_hook_handler(example_handler, NULL, NULL, APR_HOOK_LAST);
+}
+
+static int example_handler(request_rec *r)
+{
+    /* First off, we need to check if this is a call for the "example-handler" handler.
+     * If it is, we accept it and do our things, if not, we simply return DECLINED,
+     * and the server will try somewhere else.
+     */
+    if (!r->handler || strcmp(r->handler, "example-handler")) return (DECLINED);
+
+    /* Now that we are handling this request, we'll write out "Hello, world!" to the client.
+     * To do so, we must first set the appropriate content type, followed by our output.
+     */
+    ap_set_content_type(r, "text/html");
+    ap_rprintf(r, "Hello, world!");
+
+    /* Lastly, we must tell the server that we took care of this request and everything went fine.
+     * We do so by simply returning the value OK to the server.
+     */
+    return OK;
 }
