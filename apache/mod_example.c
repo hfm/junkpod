2fac77939f61322d355bb0050fc2ee17e4df5f31
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-11-20 10:19:40 +0900

    another_example_handler

--- apache/mod_example.c
+++ apache/mod_example.c
@@ -23,20 +23,24 @@ static void register_hooks(apr_pool_t *pool)
 
 static int example_handler(request_rec *r)
 {
-    /* First off, we need to check if this is a call for the "example-handler" handler.
-     * If it is, we accept it and do our things, if not, we simply return DECLINED,
-     * and the server will try somewhere else.
-     */
-    if (!r->handler || strcmp(r->handler, "example-handler")) return (DECLINED);
-
-    /* Now that we are handling this request, we'll write out "Hello, world!" to the client.
-     * To do so, we must first set the appropriate content type, followed by our output.
-     */
+    /* Set the appropriate content type */
     ap_set_content_type(r, "text/html");
+
+    /* Print out the IP address of the client connecting to us: */
     ap_rprintf(r, "Hello, world!");
 
-    /* Lastly, we must tell the server that we took care of this request and everything went fine.
-     * We do so by simply returning the value OK to the server.
-     */
+    /* If we were reached through a GET or a POST request, be happy, else sad. */
+    if ( !strcmp(r->method, "POST") || !strcmp(r->method, "GET")) {
+        ap_rputs("You used a GET or a POST method, that makes us happy!<br/>", r);
+    }
+    else {
+        ap_rputs("You did not use POST or GET, that makes us sad :(<br/>", r);
+    }
+
+    /* Lastly, if there was a query string, let's print that too! */
+    if (r->args) {
+        ap_rprintf(r, "Your query string was: %s", r->args);
+    }
+
     return OK;
 }
