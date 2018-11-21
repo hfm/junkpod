56e9fafb72d291727b13903b22cd16fd79a8fe12
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-11-21 23:19:40 +0900

    notfound

--- apache/mod_example.c
+++ apache/mod_example.c
@@ -23,24 +23,5 @@ static void register_hooks(apr_pool_t *pool)
 
 static int example_handler(request_rec *r)
 {
-    /* Set the appropriate content type */
-    ap_set_content_type(r, "text/html");
-
-    /* Print out the IP address of the client connecting to us: */
-    ap_rprintf(r, "Hello, world!");
-
-    /* If we were reached through a GET or a POST request, be happy, else sad. */
-    if ( !strcmp(r->method, "POST") || !strcmp(r->method, "GET")) {
-        ap_rputs("You used a GET or a POST method, that makes us happy!<br/>", r);
-    }
-    else {
-        ap_rputs("You did not use POST or GET, that makes us sad :(<br/>", r);
-    }
-
-    /* Lastly, if there was a query string, let's print that too! */
-    if (r->args) {
-        ap_rprintf(r, "Your query string was: %s", r->args);
-    }
-
-    return OK;
+    return HTTP_NOT_FOUND;
 }
